4596f24d6fc09c7b8f7f34f2a17c56c077b7a8e5
dragon5689 <dragon5689@gmx.net>
2013-11-30 12:15:11 +0100

    customized mergesort
    improved mergesort

--- src/Sorting/MyMergesort.java
+++ src/Sorting/MyMergesort.java
@@ -5,52 +5,66 @@ import Helper.Sorter;
 
 public class MyMergesort extends CountingSorter implements Sorter
 {
-    private int[] a;
-    private int[] b;    // Hilfsarray
-    private int n;
 
-    public void sort(int[] a)
-    {
-        this.a=a;
-        n=a.length;
-        // je nach Variante entweder/oder:
-        b=new int[c((n+1)/2)];    b=new int[c(n)];
-        mergesort(0, n-1);
-    }
+	private int[] a;
+	private int[] b;
+	private int n;
 
-    private void mergesort(int lo, int hi)
-    {
-    	incrementRekursionDepth();
-        if (lo<hi)
-        {
-            int m=(lo+hi)/2;
-            mergesort(lo, m);
-            mergesort(m+1, hi);
-            merge(lo, m, hi);
-        }
-    }
+	@Override
+	public void sort(int[] a)
+	{
+		this.a = a;
+		n = a.length;
+		b = new int[n];
+		mergesort(0, n - 1);
+	}
 
-    // Variante b
-    void merge(int lo, int m, int hi)
-    {
-    	int i, j, k;
+	private void mergesort(int lo, int hi)
+	{
+		incrementRekursionDepth();
+		if (lo < hi)
+		{
+			int m = (lo + hi) / 2;
+			mergesort(lo, m);
+			mergesort(m + 1, hi);
+			merge(lo, m, hi);
+		}
+	}
 
-        i=0; j=lo;
-        // vordere Hälfte von a in Hilfsarray b kopieren
-        while (j<=m)
-            b[c(i++)]=a[c(j++)];
+	private void merge(int lo, int m, int hi)
+	{
+		int i, j, k;
+		boolean broken = true;
 
-        i=0; k=lo;
-        // jeweils das nächstgrößte Element zurückkopieren
-        while (k<j && j<=hi)
-            if (b[c(i)]<=a[c(j)])
-                a[c(k++)]=b[c(i++)];
-            else
-                a[c(k++)]=a[c(j++)];
+		while (broken && lo <= m)
+		{
+			if (a[c(lo)] <= a[c(m + 1)])
+			{
+				lo++;
+			} else
+			{
+				broken = false;
+			}
+		}
 
-        // Rest von b falls vorhanden zurückkopieren
-        while (k<j)
-            a[c(k++)]=b[c(i++)];
-    }
+		i = 0;
+		j = lo;
+		// vordere Hälfte von a in Hilfsarray b kopieren
+		while (j <= m)
+			b[c(i++)] = a[c(j++)];
 
-}    // end class MergeSorter
+		i = 0;
+		k = lo;
+		// jeweils das nächstgrößte Element zurückkopieren
+		while (k < j && j <= hi)
+			if (b[c(i)] <= a[c(j)])
+				a[c(k++)] = b[c(i++)];
+			else
+				a[c(k++)] = a[c(j++)];
+
+		// Rest der vorderen Hälfte falls vorhanden zurückkopieren
+		while (k < j)
+			a[c(k++)] = b[c(i++)];
+	}
+
+}
